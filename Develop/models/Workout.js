3e8d33a31a124874de328b26813812bb2e009d28
William Judd <juddwilliam13@gmail.com>
2021-04-28 21:29:34 -0500

    created and populated models

---
+++ Develop/models/Workout.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now()
+    },
+
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Please enter the type of exercise."
+        },
+
+        name: {
+            type: String,
+            trim: true, 
+            required: "Please enter the name of the exercise." 
+        },
+
+        duration: {
+            type: Number,
+            required: "Please enter the duration of the exercise."
+        },
+
+        weight: {
+            type: Number
+        },
+
+        reps: {
+            type: Number
+        },
+
+        sets: {
+            type: Number
+        },
+
+        distance: {
+            type: Number
+        }
+    }]
+});
+        
+    
